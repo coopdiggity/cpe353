ef4013b86dbb02ff5098712a477e070ed48d12ab
coopdiggity <35469144+coopdiggity@users.noreply.github.com>
2019-08-29 16:29:34 -0500

    Add files via upload

---
+++ EchoDialog/dialog.cpp
@@ -0,0 +1,31 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+
+    connect(ui->clearButton, &QPushButton::clicked, ui->inputLineEdit, &QLineEdit::clear );
+
+
+
+
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_bitButton_toggled(bool checked)
+{
+    if(checked)
+        ui->bitButtonOut->setText("1");
+    else if (!checked)
+        ui->bitButtonOut->setText("0");
+
+
+}
