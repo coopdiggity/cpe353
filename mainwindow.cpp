649b460b8e66c3c26c3940e23483deb9e9c464a4
coopdiggity <35469144+coopdiggity@users.noreply.github.com>
2019-09-12 13:17:37 -0500

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,52 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QEvent>
+#include <QKeyEvent>
+#include <QtDebug>
+#include <QFile>
+#include <QTextStream>
+#include <QStringList>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+bool MainWindow::event(QEvent * e)
+{
+    if (e && e->type() == QEvent::KeyPress)
+    {
+        QKeyEvent* ke = static_cast<QKeyEvent*>(e);
+        if(ke->key() == Qt::Key_Escape)
+        {
+            qDebug() << "Escape";
+        }
+    }
+    return  QMainWindow::event(e);
+}
+
+void MainWindow::keyPressEvent(QKeyEvent * ke)
+{
+    if (ke && ke->key() == Qt::Key_Escape)
+    {
+        int count = status->text().toInt();
+        count ++;
+        status->setNum(count);
+    }
+    QMainWindow::keyPressEvent(ke);
+}
+
+void MainWindow::populateComboBox()
+{
+
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    bool event(QEvent * e);
+protected:
+    void keyPressEvent(QKeyEvent * ke);
+
+
+private:
+    Ui::MainWindow *ui;
+    QLabel* status;
+
+private slots:
+    void populateComboBox();
+};
+
+#endif // MAINWINDOW_H
